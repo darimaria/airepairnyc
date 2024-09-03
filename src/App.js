7dfa01806bd25ac42f8f2200fab462178c250d3b
Dari Dennis <ddennis@u.rochester.edu>
2024-09-03 00:04:03 -0400

    missing contact section

--- src/App.js
+++ src/App.js
@@ -1,34 +1,121 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Header from './Header';
-import InformationBoxes from './InformationBoxes';
 import logo from './assets/images/airepair_logo.png';
-import epa from './assets/images/certified-by-epa.png';
 import stockphoto from './assets/images/ac-stockphoto.jpeg';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import InformationBoxes from './InformationBoxes';
 
 function App() {
+  const [showButton, setShowButton] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
+
+  // BOTTOM TO TOP SCROLL BUTTON
+  useEffect(()=> {
+    const handleScroll = () => {
+      if(window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // NAVIGATION TOGGLE
+  const handleNavToggle = () => {
+    console.log("Nav toggle clicked");  // Add this to check if the function is being called
+    setShowLinks(!showLinks);
+  };
+
+  // FIXED NAVBAR
+
   return (
-    <body>
-      <div>
-        <Header logo={logo} certification={epa} />
-      </div>
-      <div className="bigImgContainer">
-        <img src={stockphoto} alt="stockphoto"/>
-        <div>
-          <h1 style={{"font-size":"50px"}}>Your Manhattan AC Repair Man</h1>
-          <p style={{"font-size":"30px"}}>Choose Airepair for the best quality service on demand.</p>
+    <div>
+      <header id="home">
+        <nav id="nav">
+          <div className="nav-center">
+            <div className="nav-header">
+              <img src={ logo } alt="logo" className="logo" />
+              <button className="nav-toggle" onClick={ handleNavToggle }>
+                <FontAwesomeIcon icon={ faBars } />
+              </button>
+            </div>
+            <div className={`links-container ${showLinks ? 'show-links' : ''}`}>
+              <ul className="links">
+                <li><a href="#home" className="scroll-link">Home</a></li>
+                <li><a href="#services" className="scroll-link">Services</a></li>
+                <li><a href="#about" className="scroll-link">About</a></li>
+                <li><a href="#contact" className="scroll-link">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="bigImgContainer">
+          <img src={ stockphoto } alt="stockphoto"/>
+          <div>
+            <h1 style={{ fontSize: '40px' }}>Your Manhattan AC Repair Man</h1>
+            <p style={{ fontSize: '20px' }}>Choose Airepair for the best quality service on demand.</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <h1 style={{"text-align":"center"}}>
-          We <span style={{color:"red"}}>strive </span> for your confidence and comfort. 
-          We <span style={{color:"red"}}>deliver</span> that by maintaining our 3 core values.
-        </h1>
+      </header>
+      <section id="services" className="section">
+        <div className="title">
+          <h2>Our Services</h2>
+        </div>
         <InformationBoxes />
-      </div>
-      <div>
-        <h1 style={{margin:"50px"}}>24/7 Emergency Service</h1>
-      </div>
-    </body>
+      </section>
+      <section id="about" className="section">
+        <div className="title">
+          <h2>About Us</h2>
+        </div>
+        <p>
+            Airepair is a family-owned business that is dedicated to serving Manhattan
+            and the surrounding areas with quality appliance repair services. 
+            <br />
+            <br />
+            My name is Michael and I'm a new business owner but I've got a lifetime of 
+            experience behind me. I am EPA certified and know AC systems inside and out.
+            Whether it's a quick fix, a full installation, or just some good maintenance and
+            advice, I've got you covered. I specialize in providing personalized, reliable 
+            handyman services to both homes and businesses right here in Manhattan.
+            <br />
+            <br />
+            Why choose Airepair? Because I'm a straight shooter who takes pride in a job well
+            done. I believe in showing up on time, getting the work done right the first time and
+            tresting your space like it's my own.
+            <br />
+            <br />
+            So, if you need someone who combines old-school work ethic with top-notch expertise, 
+            give me a call. I'm Michael, you go-to-guy for all things AC.
+          </p>
+      </section>
+      <section id="contact" className="section">
+        <div className="title">
+          <h2>Contact Us</h2>
+        </div>
+      </section>
+      <footer>
+        <div className="footer-center">
+          <p>copyright &copy; Airepair
+            <span className="date">2021</span>
+            .all rights reserved
+          </p>
+        </div>
+      </footer>
+      <a href="#home" className="scroll-link top-link"> 
+        {showButton && (
+          <button className="button-up" id="scrollButton">
+          <FontAwesomeIcon icon={ faArrowUp } />
+        </button>
+        )}
+      </a>
+    </div>
   );
 }
 
--- src/InformationBoxes.js
+++ src/InformationBoxes.js
@@ -5,16 +5,58 @@ const InformationBoxes = () => {
     return(
         <div className="infoBoxContainer">
             <div>
-                <h1>Header</h1>
-                <p>Content</p>
+                <h1>Repairs</h1>
+                <ul>
+                    <li>Refrigerator Repair: Fast and efficient service for all refrigerator issues,
+                        ensuring your food stays fresh.
+                    </li>
+                    <li>Air Conditioner Repair: Comprehensive repairs for all types of 
+                        air conditioning units, providing you comfort in every season.
+                    </li>
+                    <li>Oven Repair: Timely repairs for electric ovens so you can get back to cooking your
+                        favorite meals.
+                    </li>
+                    <li>
+                        Small Appliance Repair: Expert diagnosis and repair for various small appliances
+                        to keep them running smoothly.
+                    </li>
+                </ul>
             </div>
             <div>
-                <h1>Header</h1>
-                <p>Content</p>
+                <h1>Installations</h1>
+                <ul>
+                    <li>
+                        Air Conditioner Installation: Expert installation of air conditioning units, tailored to
+                        your home's needs.
+                    </li>
+                    <li>
+                        Portable AC Installation: Professional installation services for portable air conditioners,
+                    </li>
+                    <li>
+                        Oven Installation: Professional installation services for all oven models, ensuring
+                        safe and optimal performance.
+                    </li>
+                </ul>
             </div>
             <div>
-                <h1>Header</h1>
-                <p>Content</p>
+                <h1>Assembly</h1>
+                <ul>
+                    <li>
+                        Bookcase assembly
+                    </li>
+                    <li>
+                        Bedframe assembly 
+                    </li>
+                    <li>
+                        Desk assembly
+                    </li>
+                    <li>
+                        Cabinet assembly
+                    </li>
+                    <li>
+                        Anything you can think of!
+                    </li>
+                </ul>
             </div> 
         </div>
     )
